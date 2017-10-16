968634597ba077abbbbf871acb566af1ef53fd91
Unknown <manumathewthomas@gmail.com>
2017-10-16 18:27:23 -0500

    obstacle avoidance

--- src/js/main.js
+++ src/js/main.js
@@ -6,8 +6,11 @@
   let controls;
   let scene;
   let renderer;
-  let target = new THREE.Vector3(0, 0, 0);
+  let target = new THREE.Vector3(250, 0, 0);
   let mouse = {x: 0, y: 0};
+  let objects = [];
+  let obstacleMesh; 
+  let obstacleGeometry;
 
 
   let boids = [];
@@ -17,16 +20,16 @@
   	constructor() {
   		this.velocity = new THREE.Vector3(0, 0, 0);
   		this.acceleration = new THREE.Vector3(0, 0, 0);
-  		this.maxSpeed = 4;
-  		this.maxForce = 0.1;
-  		this.geometry = new THREE.ConeGeometry( 5, 20, 32 );
+  		this.maxSpeed = 0.5;
+  		this.maxForce = 0.2;
+  		this.geometry = new THREE.ConeGeometry( 3, 5, 32 );
   		this.axis = new THREE.Vector3(0, 1, 0);
 		this.material = new THREE.MeshBasicMaterial( {color: 0xf03b20} );
-		this.mesh = new THREE.Mesh(this.geometry, this.material );  	
+		this.mesh = new THREE.Mesh(this.geometry, this.material );  
+		this.collision = false;	
 
 	}
   }
-
 
   let createBoid = function(n) {
 
@@ -36,26 +39,51 @@
 
   	 _.forEach(boids, function(d){
   	 	d.mesh.rotateZ(-Math.PI / 2 );
-  	 	d.mesh.position.set(Math.floor(Math.random() * 400) + 1  , 0, Math.floor(Math.random() * 400) + 1  );
+    	d.mesh.position.set( Math.floor(Math.random() * 200) -400 , 10 , Math.floor(Math.random() * 400) -200 );
     	scene.add(d.mesh);
     });
+
+  }
+
+  let createScene = function() {
+
+    let tableGeometry = new THREE.BoxGeometry( 400, 1, 400 );
+    let tableMaterial = new THREE.MeshBasicMaterial( {color: 0x636363} );
+    let tableMesh = new THREE.Mesh( tableGeometry, tableMaterial );
+    tableMesh.position.set(0,-5,0);
+	objects.push(tableMesh);
+    scene.add( tableMesh );
+
+    obstacleGeometry = new THREE.BoxGeometry( 50, 50, 50 );
+	obstacleGeometry.computeBoundingSphere();
+    let obstacleMaterial = new THREE.MeshBasicMaterial( {color: 0xaddd8e} );
+    obstacleMesh = new THREE.Mesh( obstacleGeometry, obstacleMaterial );
+    obstacleMesh.position.set(0,25,0);
+    scene.add( obstacleMesh );
+
+	var cubeGeometry = new THREE.BoxGeometry( 50, 50, 50 );
+	var cubeMaterial = new THREE.MeshLambertMaterial( { color: 0x00ff80, overdraw: 0.5 } );
 
   }
  
   let init = function() {
 
     camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
-    camera.position.set( 0, 0 , 600 );
+    camera.position.set(0, 400, 0);
+
 
     controls = new THREE.OrbitControls( camera );
 
     scene = new THREE.Scene();
     scene.background = new THREE.Color( 0xeeeeee );
 
-    let axisHelper = new THREE.AxisHelper(500);
-    scene.add( axisHelper );
 
-    scene.add( new THREE.GridHelper( 400, 10 ) );
+    createScene();
+
+    // let axisHelper = new THREE.AxisHelper(500);
+    // scene.add( axisHelper );
+
+    // scene.add( new THREE.GridHelper( 400, 10 ) );
 
   
 
@@ -67,7 +95,7 @@
     document.body.appendChild( renderer.domElement );
 
     window.addEventListener( 'resize', onWindowResize, false );
-    document.addEventListener('mousemove', onMouseMove, false);
+    document.addEventListener('click', onMouseMove, false);
 
     /* animate the scene */
 
@@ -132,7 +160,7 @@
   }
 
   let seperate = function(boid) {
-  	let seperationDistance = 50;
+  	let seperationDistance = 10;
   	let sum = new THREE.Vector3();
   	let steer = new THREE.Vector3();
   	let count = 0;
@@ -189,29 +217,37 @@
   	return steer;
   }
 
-  let update = function(boid) {
+  let obstacle = function(boid) {
 
+	let raycaster = new THREE.Raycaster();
+	let steer = new THREE.Vector3();
+	raycaster.set(boid.mesh.position, boid.velocity.clone().normalize());
+	let d = raycaster.intersectObject(obstacleMesh);
+	if(d[0] && d[0].distance>0 && d[0].distance < 50)
+	{
+		steer = new THREE.Vector3().subVectors(d[0].face.normal, boid.velocity);
+		steer.clampLength(steer.length(), boid.maxForce);
+		boid.collision = true;
+	}
+	else
+	boid.collision = false;
+		
+	return steer;
+  }
+
+  let update = function(boid) {
+		
   	boid.velocity.add(boid.acceleration);
   	boid.velocity.clampLength(boid.velocity.length(), boid.maxSpeed);
-  	// boid.mesh.lookAt(boid.velocity);
+
+    boid.mesh.quaternion.setFromUnitVectors(boid.axis, boid.velocity.clone().normalize());
 	
-	boid.mesh.quaternion.setFromUnitVectors(boid.axis, boid.velocity.clone().normalize());
   	boid.mesh.position.add(boid.velocity);
+	
   	boid.acceleration.set(0, 0, 0);
   }
 
   let onMouseMove = function(event) {
-  	event.preventDefault();
-  	
-	mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-	mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
-
-	let vector = new THREE.Vector3(mouse.x, mouse.y, 0.5);
-	vector.unproject( camera );
-	let dir = vector.sub( camera.position ).normalize();
-	let distance = - camera.position.z / dir.z;
-	let pos = camera.position.clone().add( dir.multiplyScalar( distance ) );
-	target = pos;
 	
   } 
 
@@ -222,22 +258,29 @@
   		let seperateForce = new THREE.Vector3();
   		let alignmnetForce = new THREE.Vector3();
   		let cohesionForce = new THREE.Vector3();
+		let obstacleForce = new THREE.Vector3();
   		let seekForce = new THREE.Vector3();
+	
   	
   		seperateForce = seperate(d);
   		alignmnetForce = alignment(d);
   		cohesionForce = cohesion(d);
+		obstacleForce = obstacle(d);
   		seekForce = seek(d, target);
 
-  		seperateForce.multiplyScalar(1);
+
+  		seperateForce.multiplyScalar(0.1);
   		alignmnetForce.multiplyScalar(1);
   		cohesionForce.multiplyScalar(1);
+		obstacleForce.multiplyScalar(5);
   		seekForce.multiplyScalar(1);
 
   		applyForce(d, seperateForce);
   		applyForce(d, alignmnetForce);
   		applyForce(d, cohesionForce);
-  		applyForce(d, seekForce);
+		applyForce(d, obstacleForce);
+		if(!d.collision)
+			applyForce(d, seekForce);
 
 
   		update(d);
